2da0a06894cc3d50be3a85b179522f6775ddd5bb
dmootzler <dmetzler@ucsd.edu>
2016-12-08 18:49:28 -0800

    Changed audio session category for playback init

--- src/ios/CDVSound.m
+++ src/ios/CDVSound.m
@@ -192,10 +192,11 @@
 {
     BOOL bSession = YES;
 
-    //if (!self.avSession) {
+    if (!self.avSession) {
         NSError* error = nil;
 
-        self.avSession = [[AVAudioSession sharedInstance] setCategory: AVAudioSessionCategoryPlayAndRecord withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker error:nil];
+        self.avSession = [AVAudioSession sharedInstance] 
+        [self.avSession setCategory: AVAudioSessionCategoryPlayAndRecord withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker error:nil];
      
         if (error) {
             // is not fatal if can't get AVAudioSession , just log the error
@@ -203,7 +204,7 @@
             self.avSession = nil;
             bSession = NO;
         }
-   // }
+    }
     return bSession;
 }
 
@@ -348,8 +349,8 @@
                     bPlayAudioWhenScreenIsLocked = [playAudioWhenScreenIsLocked boolValue];
                 }
 
-                NSString* sessionCategory = bPlayAudioWhenScreenIsLocked ? AVAudioSessionCategoryPlayback : AVAudioSessionCategorySoloAmbient;
-                [self.avSession setCategory:sessionCategory error:&err];
+                NSString* sessionCategory = AVAudioSessionCategoryPlayAndRecord;
+                [self.avSession setCategory:sessionCategory withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker error:&err];
                 if (![self.avSession setActive:YES error:&err]) {
                     // other audio with higher priority that does not allow mixing could cause this to fail
                     NSLog(@"Unable to play audio: %@", [err localizedFailureReason]);
